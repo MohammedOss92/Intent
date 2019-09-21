98e86bd6e05f9a4a0160cff0ecbcbeb1ab08abfb
MohammedOss92 <abdallahomar1992@gmail.com>
2019-09-21 21:59:54 +0300

    Hiii

---
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    lateinit var editText : EditText
+
+    lateinit var btn : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.button)
+        editText = findViewById(R.id.firstname)
+
+        btn.setOnClickListener {
+            var intent: Intent = Intent(applicationContext, Main2Activity::class.java)
+            var firstname = editText.text.toString()
+            intent.putExtra("firstname", firstname)
+            startActivity(intent)
+        }
+    }
+}
